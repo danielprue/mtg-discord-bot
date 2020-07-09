20c8428d05340be4b9e361db9047317338eab38c
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-07-09 16:39:54 -0600

    added set filter to queries

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const { token, prefix, scryfall } = require('./config.json');
+const { prefix, scryfall } = require('./config.json');
 const Axios = require('axios');
 
 client.on('ready', () => {
   console.log('MTG_CARD_FETCHER is online');
 });
 
+const queries = new Set(`${prefix}set`);
+
 client.on('message', (msg) => {
   if (msg.content.startsWith(`${prefix}card`)) {
     let search = scryfall;
     const cardName = msg.content.split(' ');
     for (let i = 1; i < cardName.length; i++) {
-      search += cardName[i];
-      i != cardName.length - 1 ? (search += '+') : null;
+      if (cardName[i] in queries) {
+        search = search + '&' + cardName[i].slice(1) + '=';
+      } else {
+        search += cardName[i];
+        i != cardName.length - 1 ? (search += '+') : null;
+      }
     }
 
     Axios.get(search)
